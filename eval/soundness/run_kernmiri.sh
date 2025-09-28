043914c41a79545c2de2de88256c36e05f89f780
zjp <jiping_zhou@foxmail.com>
2025-09-28 06:40:27 +0000

    fix(eval/soundness/run_kernmiri.sh): avoid make build

--- eval/soundness/run_kernmiri.sh
+++ eval/soundness/run_kernmiri.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 set -eou pipefail
-apt-get install -y iperf iperf3 nginx sysbench exfat-fuse
+
+# Add dependencies that are required by `make build`.
+# apt-get install -y iperf iperf3 nginx sysbench exfat-fuse
 
 #if [ -d "$HOME/miri" ]; then
 #    rm -rf $HOME/miri
@@ -31,7 +33,12 @@ pushd ~/miri_asterinas
 git reset --hard 59dca48f8b2d9d3e5edd8ef89443417b57749682
 rustup override set nightly-2024-11-04
 make install_osdk
-# make build
+
+# make build # No need to build the project
+#
+# cargo osdk early checks this file, so this trick avoids downloading or performing
+# things that are irrelevant to KernMiri.
+mkdir -p test/build && touch test/build/initramfs.cpio.gz
 
 pushd ostd
 MIRIFLAGS="-Zmiri-disable-stacked-borrows -Zmiri-ignore-leaks" cargo osdk miri run
